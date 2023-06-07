8ebec8b02899addf3df5726e3289e1ec75c4dae2
chencho2003 <56181309+chencho2003@users.noreply.github.com>
2023-06-07 21:40:44 +0600

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,20 +1,22 @@
-import mysql from "mysql";
+import dotenv from 'dotenv';
+import mysql from 'mysql';
 
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: process.env.RAILWAY_DATABASE_HOST || "localhost",
-  user: process.env.RAILWAY_DATABASE_USER || "root",
-  password: process.env.RAILWAY_DATABASE_PASSWORD || "",
-  database: process.env.RAILWAY_DATABASE_NAME || "railway",
+  host: process.env.RAILWAY_DATABASE_HOST || 'localhost',
+  user: process.env.RAILWAY_DATABASE_USER || 'root',
+  password: process.env.RAILWAY_DATABASE_PASSWORD || '',
+  database: process.env.RAILWAY_DATABASE_NAME || 'railway',
   port: process.env.RAILWAY_DATABASE_PORT || 3306,
 });
 
 db.connect((err) => {
   if (err) {
-    console.error("Error connecting to the database:", err);
+    console.error('Error connecting to the database:', err);
     return;
   }
-  console.log("Connected to the database");
+  console.log('Connected to the database');
 });
 
 export default db;
