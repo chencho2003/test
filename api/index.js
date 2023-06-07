d03005f5bde6fc56449dac2e047b19e61abfced4
chencho2003 <56181309+chencho2003@users.noreply.github.com>
2023-06-07 21:40:26 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/users.js";
 import cors from "cors";
-require("dotenv").config();
 const app = express();
 
 app.use(express.json());
